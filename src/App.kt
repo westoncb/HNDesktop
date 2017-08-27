ccad23408e50a39fcd11d79a5ea49f97359c3f52
Weston C. Beecroft <westoncb@gmail.com>
2017-08-27 12:00:01 -0700

    Initial layout attempt + progress bar

--- src/App.kt
+++ src/App.kt
@@ -1,10 +1,9 @@
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.google.gson.JsonPrimitive
 import javafx.application.Platform
-import java.awt.Dimension
 import javax.swing.*
-import java.awt.Toolkit
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 import java.io.InputStreamReader
@@ -15,18 +14,21 @@ import sun.jvm.hotspot.utilities.soql.SOQLEngine.getEngine
 import javafx.scene.Scene
 import javafx.scene.web.WebView
 import javafx.embed.swing.JFXPanel
-
-
+import java.awt.*
+import java.util.*
+import javax.swing.border.BevelBorder
 
 
 /**
  * Created by weston on 8/26/17.
  */
 
+val progressBar = JProgressBar(0, 30)
 
 fun main(args: Array<String>) {
-    MetalLookAndFeel.setCurrentTheme(DefaultMetalTheme())
-    UIManager.setLookAndFeel(MetalLookAndFeel())
+//    MetalLookAndFeel.setCurrentTheme(DefaultMetalTheme())
+//    UIManager.setLookAndFeel(MetalLookAndFeel())
+    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 
 
     val frame = JFrame("Hacker News")
@@ -35,8 +37,16 @@ fun main(args: Array<String>) {
 
     val contentPane = JPanel()
     contentPane.layout = BoxLayout(contentPane, BoxLayout.Y_AXIS)
+
+    progressBar.preferredSize = Dimension(300, 100)
+    progressBar.minimumSize = Dimension(300, 100)
+    progressBar.maximumSize = Dimension(300, 100)
+    progressBar.value = 0
+//    progressBar.isStringPainted = true
+    contentPane.add(progressBar)
+
     val scroller = JScrollPane(contentPane)
-    scroller.verticalScrollBar.unitIncrement = 20
+    scroller.verticalScrollBar.unitIncrement = 16
     frame.contentPane.add(scroller)
 
     frame.setLocation(screenSize.width / 2 - frame.size.width / 2, screenSize.height / 2 - frame.size.height / 2)
@@ -45,6 +55,8 @@ fun main(args: Array<String>) {
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
     val stories = getFrontPageStories()
+
+    contentPane.remove(progressBar)
 
     for (story in stories) {
         contentPane.add(getStoryPanel(story))
@@ -62,19 +74,50 @@ fun main(args: Array<String>) {
     frame.revalidate()
 }
 
-fun getStoryPanel(story : JsonElement) : JPanel {
-    val panel = JPanel()
-    panel.border = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), story.asJsonObject.get("title").asString)
-    panel.minimumSize = Dimension(500, 100)
-    panel.preferredSize = Dimension(500, 100)
+fun getStoryPanel(story : JsonObject) : JPanel {
+    val title = story.get("title").asString
+    val points = story.get("score").asInt
+    val time = Date(story.get("time").asLong * 1000).toString()
+    val kids = story.get("kids")?.asJsonArray
+    val numKids = kids?.size()
+    val user = story.get("by").asString
+
+    val panel = JPanel(GridBagLayout())
+    val titleBorder = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), title)
+    panel.border = BorderFactory.createCompoundBorder(titleBorder, BorderFactory.createEmptyBorder(4, 4, 4, 4))
+
+    val titleButton = JButton("Go")
+    val discussButton = JButton("Discuss ($numKids)")
+    val pointsLabel = JLabel("$points points")
+    val userButton = JButton(" by $user")
+    val timeLabel = JLabel("$time")
+
+    val c = GridBagConstraints()
+    c.anchor = GridBagConstraints.FIRST_LINE_START
+    c.fill = GridBagConstraints.HORIZONTAL
+
+    c.gridx = 0
+    c.gridy = 0
+    panel.add(titleButton, c)
+    c.gridx = 1
+    panel.add(discussButton, c)
+    c.gridx = 2
+    panel.add(userButton, c)
+    c.gridy = 1
+    c.gridx = 0
+    c.anchor = GridBagConstraints.LAST_LINE_START
+    panel.add(pointsLabel, c)
+    c.gridx = 1
+    c.anchor = GridBagConstraints.LAST_LINE_END
+    panel.add(timeLabel, c)
 
     return panel
 }
 
-fun getFrontPageStories() : ArrayList<JsonElement> {
+fun getFrontPageStories() : ArrayList<JsonObject> {
     val storyIDs = treeFromURL("https://hacker-news.firebaseio.com/v0/topstories.json")
     val iter = storyIDs.asJsonArray.iterator()
-    val stories = ArrayList<JsonElement>()
+    val stories = ArrayList<JsonObject>()
 
     var count : Int = 0
     while (iter.hasNext()) {
@@ -82,10 +125,11 @@ fun getFrontPageStories() : ArrayList<JsonElement> {
 
         val story = getStory(id)
 
-        stories.add(story)
+        stories.add(story.asJsonObject)
         count++
+        progressBar.value = count
 
-        if (count > 8)
+        if (count > 29)
             break
     }
 
